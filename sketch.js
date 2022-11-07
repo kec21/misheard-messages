0d06b7c2514e015d6095858be999486dcb5875d4
Kat Chudy <kec21@fsu.edu>
2022-11-07 13:56:00 -0500

    initial commit with copied note

--- sketch.js
+++ sketch.js
@@ -1,3 +1,6 @@
+//I want to build off of the idea of a hearing loss or deafness simulator by adding an element of humor. I have been keeping several lists in my sketchbooks, on notes on my phone, and on post it notes of all the misheard words and phrases that have struck me as humorous. I was also thinking about finding a way to tie this back to an idea I had in my bookmaking class about thaumatropes. I had wanted to make visuals that melded together to visually talk about the way speech gets garbled and how my brain struggles to come up with possible matches, even when those words do not fit with the conversation. I want to play with the screen reader in conjunction with this, and will start experimenting with images and words. I will start by collecting the lists and selecting the most interesting or funny words pairings.
+
+
 function setup() {
   createCanvas(400, 400);
 }
