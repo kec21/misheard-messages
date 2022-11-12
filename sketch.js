6cb4a0e07860c195d11e43130257a641652c3d11
Kat Chudy <kec21@fsu.edu>
2022-11-12 12:11:18 -0500

    kind of working scratch off

--- sketch.js
+++ sketch.js
@@ -1,27 +1,32 @@
-//I want to build off of the idea of a hearing loss or deafness simulator by adding an element of humor. I have been keeping several lists in my sketchbooks, on notes on my phone, and on post it notes of all the misheard words and phrases that have struck me as humorous. I was also thinking about finding a way to tie this back to an idea I had in my bookmaking class about thaumatropes. I had wanted to make visuals that melded together to visually talk about the way speech gets garbled and how my brain struggles to come up with possible matches, even when those words do not fit with the conversation. I want to play with the screen reader in conjunction with this, and will start experimenting with images and words. I will start by collecting the lists and selecting the most interesting or funny words pairings.
-
-
-
 let img;
+let stamp;
 let topLayer;
 
 function preload() {
-  img = loadImage(image.png);
+  img = loadImage("lettuce.jpeg");
 }
 
-function setup(){
-  createCanvas(windowWidth, windowHeight);
-  topLayer = createGraphics(w, h);
-
+function setup() {
+  createCanvas(600, 600);
+  topLayer = createGraphics(width, height);
+  
   topLayer.background(200);
   topLayer.textSize(50);
   topLayer.textAlign(CENTER);
-  topLayer.text("SCRATCH ME", width/2, height/2);
-
+  topLayer.text("TEXT US", width/2, height/2);
+    
+  topLayer.imageMode(CENTER);
   topLayer.strokeWeight(40);
-  topLayer.blendMode(REMOVE);
+  topLayer.blendMode(REMOVE);  
 }
 
 function draw() {
-  background(100)
+  image(img, 0, 0, width, height);
+  
+  if(mouseIsPressed) {
+    topLayer.line(pmouseX, pmouseY, mouseX, mouseY);
+    // topLayer.image(stamp, mouseX, mouseY, stamp.width * 2, stamp.height*2);
+  }
+  
+  image(topLayer, 0, 0);
 }
